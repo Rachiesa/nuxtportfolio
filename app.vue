fccbd2461122d2089033b7351e9ce659a6bff4d7
Rachiesa Yazid Audi <audichiesa99@gmail.com>
2025-06-11 12:45:12 +0700

    new

---
+++ app.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <nav>
+      <div
+        class="flex fixed inset-x-0 justify-center md:justify-end top-0 md:right-0 z-10 text-tertiary font-anton md:text-xl p-1 gap-3">
+        <div>
+          <button @click="web('react')" class="">/About</button>
+        </div>
+        <div>
+          <button @click="scrollTo('#projects')" class="">/Projects</button>
+        </div>
+        <div>
+          <button @click="scrollTo('#contact')" class="">/Contact</button>
+        </div>
+      </div>
+      <div class="flex h-screen w-screen">
+        <div class="flex justify-end items-center w-1/3 bg-secondary text-tertiary">
+          <h1 id="R" class="font-anton text-[200px] fixed z-10 p-3">R</h1>
+        </div>
+        <div class="flex justify-center items-center w-1/3 bg-primary text-tertiary">
+          <h1 id="Y" class="font-anton text-[200px] z-2">Y</h1>
+        </div>
+        <div class="flex justify-start items-center w-1/3 bg-secondary text-tertiary">
+          <h1 id="A" class="font-anton text-[200px] fixed z-10 p-3">A</h1>
+        </div>
+      </div>
+    </nav>
+    <NuxtRouteAnnouncer />
+
+    <About />
+
+
+    <div id="projects" class="h-screen w-screen bg-blue-400">
+      <h1>Projects</h1>
+    </div>
+    <div id="contact" class="h-screen w-screen bg-green-400">
+      <h1>Contact</h1>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { gsap } from "gsap";
+import ScrollToPlugin from "gsap/src/ScrollToPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import About from "./components/about.vue";
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollToPlugin);
+const scrollTo = (id) => {
+  gsap.to(window, { duration: 1, scrollTo: id });
+};
+
+
+
+onMounted(() => {
+  gsap.set("#R", { duration: 0, x: -500, opacity: 0 });
+  gsap.to("#R", { duration: 2, x: 0, opacity: 1 });
+  gsap.set("#Y", { duration: 0, y: 500, opacity: 0 });
+  gsap.to("#Y", { delay: 0.5, duration: 2, y: 0, opacity: 1 });
+  gsap.set("#A", { duration: 0, x: 500, opacity: 0 });
+  gsap.to("#A", { delay: 1, duration: 2, x: 0, opacity: 1 });
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#about",
+      start: "top center",
+      end: "bottom center",
+      toggleActions: "play reverse play reverse",
+      scrub: false,
+
+    }
+  });
+
+  tl.to("#aboutitem", { y: -50, opacity: 1, duration: 1 },)
+
+  const itl = gsap.timeline({ repeat: -1 });
+  itl.to("#react", { y: -2, duration: 0.1 },)
+    .to("#react", { y: 0, duration: 0.1 },)
+    .to("#tailwind", { y: -2, duration: 0.1 },)
+    .to("#tailwind", { y: 0, duration: 0.1 },)
+    .to("#js", { y: -2, duration: 0.1 },)
+    .to("#js", { y: 0, duration: 0.1 },)
+    .to("#html", { y: -2, duration: 0.1 },)
+    .to("#html", { y: 0, duration: 0.1 },)
+    .to("#css", { y: -2, duration: 0.1 },)
+    .to("#css", { y: 0, duration: 0.1 },)
+    .to("#photoshop", { y: -2, duration: 0.1 },)
+    .to("#photoshop", { y: 0, duration: 0.1 },)
+    .to("#illustrator", { y: -2, duration: 0.1 },)
+    .to("#illustrator", { y: 0, duration: 0.1 },)
+    .to("#aftereffects", { y: -2, duration: 0.1 },)
+    .to("#aftereffects", { y: 0, duration: 0.1 },)
+    .to("#email", { y: -2, duration: 0.1 },)
+    .to("#email", { y: 0, duration: 0.1 },)
+    .to("#github", { y: -2, duration: 0.1 },)
+    .to("#github", { y: 0, duration: 0.1 },)
+    .to("#linkedin", { y: -2, duration: 0.1 },)
+    .to("#linkedin", { y: 0, duration: 0.1 },)
+});
+</script>
---
+++ components/About.vue
@@ -0,0 +1,96 @@
+<template>
+    <section id='about' class='flex h-screen w-screen '>
+        <div class='w-1/3 bg-secondary hidden md:block'></div>
+        <div class='flex flex-col w-screen md:w-1/3 bg-primary text-tertiary font-anton p-1 gap-3'>
+            <div class='flex justify-end items-end text-xl h-4/12 z-2'>
+                <h2 id='aboutitem' class='opacity-0'>HI, IM RACHIESA YAZID AUDI</h2>
+            </div>
+            <div class=' h-3/12 text-7xl z-2'>
+                <h1 id='aboutitem' class='opacity-0'>FRONT-END</h1>
+                <h1 id='aboutitem' class='opacity-0'>DEVELOPER</h1>
+            </div>
+            <div class='flex flex-col gap-2 h-7/12 z-2'>
+                <p id='aboutitem' class='opacity-0 font-trispace'>I AM AN INFORMATICS ENGINEERING STUDENT WITH
+                    EXPERIENCE IN
+                    BOTH DESIGN & PROGRAMMING. FAMILIAR WITH ADOBE PHOTOSHOP, ILLUSTRATOR, AND FIGMA FOR UI/UX AND
+                    GRAPHIC DESIGN. ALSO FAMILIAR WITH HTML & CSS, AND JAVASCRIPT.
+                </p>
+                <div id='aboutitem' class=' opacity-0 flex p-1 h-1/2'>
+                    <div class='grid grid-cols-5 gap-1'>
+                        <img @click="web('react')" id='react' src="../assets/icon/ReactLogo.svg" alt="React"
+                            class=' hover:animate-pulse rounded-md '></img>
+                        <img @click="web('tailwind')" id='tailwind' src="../assets/icon/TailwindLogo.svg" alt="Tailwind"
+                            class='hover:animate-pulse rounded-md'></img>
+                        <img @click="web('js')" id='js' src="../assets/icon/JSLogo.svg" alt="JavaScript"
+                            class='hover:animate-pulse rounded-md'></img>
+                        <img @click="web('html')" id='html' src="../assets/icon/HTMLLogo.svg" alt="HTML"
+                            class='hover:animate-pulse rounded-md'></img>
+                        <img @click="web('css')" id='css' src="../assets/icon/CSSLogo.svg" alt="CSS"
+                            class='hover:animate-pulse rounded-md'></img>
+                        <img alt="React" class='invisible'></img>
+                        <img @click="web('photoshop')" id='photoshop' src="../assets/icon/PhotoshopLogo.svg"
+                            alt="Photoshop" class='hover:animate-pulse rounded-md'></img>
+                        <img @click="web('illustrator')" id='illustrator' src="../assets/icon/IllustratorLogo.svg"
+                            alt="Illustrator" class='hover:animate-pulse
+                        rounded-md'></img>
+                        <img @click="web('aftereffects')" id='aftereffects' src="../assets/icon/AfterEffectsLogo.svg"
+                            alt="AfterEffects" class='hover:animate-pulse
+                        rounded-md'></img>
+                        <img alt="React" class='invisible'></img>
+                        <img @click="web('email')" id='email' src="../assets/icon/MailLogo.svg" alt="Email"
+                            class='hover:animate-pulse rounded-md'></img>
+                        <img alt="React" class='invisible'></img>
+                        <img @click="web('github')" id='github' src="../assets/icon/GitHubLogo.svg" alt="Github"
+                            class='hover:animate-pulse rounded-md'></img>
+                        <img alt="React" class='invisible'></img>
+                        <img id='linkedin' src="../assets/icon/LinkedinLogo.svg" alt="Linkedin"
+                            class='hover:animate-pulse rounded-md'></img>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class='w-1/3 bg-secondary hidden md:block'></div>
+    </section>
+</template>
+
+<script setup>
+const web = (web) => {
+    switch (web) {
+        case 'react':
+            window.open('https://reactjs.org/', '_blank')
+            break;
+        case 'tailwind':
+            window.open('https://tailwindcss.com/', '_blank')
+            break;
+        case 'js':
+            window.open('https://developer.mozilla.org/en-US/docs/Web/JavaScript', '_blank')
+            break;
+        case 'html':
+            window.open('https://developer.mozilla.org/en-US/docs/Web/HTML', '_blank')
+            break;
+        case 'css':
+            window.open('https://developer.mozilla.org/en-US/docs/Web/CSS', '_blank')
+            break;
+        case 'photoshop':
+            window.open('https://www.adobe.com/products/photoshop.html', '_blank')
+            break;
+        case 'illustrator':
+            window.open('https://www.adobe.com/products/illustrator.html', '_blank')
+            break;
+        case 'aftereffects':
+            window.open('https://www.adobe.com/products/aftereffects.html', '_blank')
+            break;
+        case 'email':
+            window.open("https://mail.google.com/mail/?view=cm&fs=1&to=audichiesa99@gmail.com&su=Hello,Rachiesa let's talk", '_blank')
+            break;
+        case 'github':
+            window.open('https://github.com/Rachiesa', '_blank')
+            break;
+        case 'linkedin':
+            window.open('https://www.linkedin.com/in/audichiesa/', '_blank')
+            break;
+        default:
+            break;
+    }
+}
+</script>
